ce1a29b8edca701d9b7204a6a81a3c85ae86d8da
Minggul_ <pmq0101@gmail.com>
2022-10-05 14:41:59 +0900

    [update] 셀레니움 문법 변경 및 이름 자동 입력 기능 추가

--- 성덕원에서 짠 코드/지금 쓰고 있는 코드/이름 자동 확인 여러 명.py
+++ 성덕원에서 짠 코드/지금 쓰고 있는 코드/이름 자동 확인 여러 명.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import warnings
-
+import get_name_list
 warnings.filterwarnings("ignore")
 
 def is_xpath_exist(dr, xpath):
@@ -54,35 +54,35 @@ def input_date(op):
 def name_auto_check(driver, name):
     # 입소자 명단 클릭
     is_xpath_exist(driver, '//*[@id="content"]/div/div[1]/div[2]/div[2]/span/span[1]/span/span[2]')
-    driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div[2]/div[2]/span/span[1]/span/span[2]').click()
+    driver.find_element('xpath', '//*[@id="content"]/div/div[1]/div[2]/div[2]/span/span[1]/span/span[2]').click()
 
     # 이름 입력
     is_xpath_exist(driver, '/html/body/span/span/span[1]/input')
-    driver.find_element_by_xpath('/html/body/span/span/span[1]/input').send_keys(name + Keys.ENTER)
+    driver.find_element('xpath', '/html/body/span/span/span[1]/input').send_keys(name + Keys.ENTER)
 
     # 수분 버튼 클릭
-    if driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div[1]/div[1]/h2').text == "경관식 대상자":
+    if driver.find_element('xpath', '//*[@id="content"]/div/div[1]/div[1]/div[1]/h2').text == "경관식 대상자":
         driver.find_element(By.XPATH, '//*[@id="content"]/div/div[1]/div[1]/div[2]/div/button').click()
 
     # 시작 날짜 박스 클릭
     is_xpath_exist(driver, '//*[@id="start_dt"]')
 
-    driver.find_element_by_xpath('//*[@id="start_dt"]').click()
-    driver.find_element_by_xpath('//*[@id="ui-datepicker-div"]/div[1]/div/select[2]').click()
+    driver.find_element('xpath', '//*[@id="start_dt"]').click()
+    driver.find_element('xpath', '//*[@id="ui-datepicker-div"]/div[1]/div/select[2]').click()
 
     # 시작 날짜 클릭
-    Select(driver.find_element_by_xpath('//*[@id="ui-datepicker-div"]/div[1]/div/select[2]')).select_by_index(
+    Select(driver.find_element('xpath', '//*[@id="ui-datepicker-div"]/div[1]/div/select[2]')).select_by_index(
         str(start_month))
-    driver.find_element_by_link_text(str(start_date)).click()
+    driver.find_element(By.LINK_TEXT,str(start_date)).click()
 
     # 종료 날짜 박스 클릭
-    driver.find_element_by_xpath('//*[@id="end_dt"]').click()
-    driver.find_element_by_xpath('//*[@id="ui-datepicker-div"]/div[1]/div/select[2]').click()
+    driver.find_element('xpath', '//*[@id="end_dt"]').click()
+    driver.find_element('xpath', '//*[@id="ui-datepicker-div"]/div[1]/div/select[2]').click()
 
     # 종료 날짜 클릭
-    Select(driver.find_element_by_xpath('//*[@id="ui-datepicker-div"]/div[1]/div/select[2]')).select_by_index(
+    Select(driver.find_element('xpath', '//*[@id="ui-datepicker-div"]/div[1]/div/select[2]')).select_by_index(
         str(end_month))
-    driver.find_element_by_link_text(str(end_date)).click()
+    driver.find_element(By.LINK_TEXT,str(end_date)).click()
 
     # 검색 버튼 클릭
     driver.find_element(By.XPATH, '//*[@id="content"]/div/div[1]/div[2]/div[4]/button''[1]').click()
@@ -102,15 +102,15 @@ def name_auto_check(driver, name):
 
     # 끝 이름 첫 이름 비교
     for i in range(1, tmp):
-        name_list = driver.find_element_by_xpath(
+        name_list = driver.find_element('xpath',
             '//*[@id="content"]/div/div[2]/div/table/tbody/tr[{}]/td[7]'.format(i)).text.split()
         last_name = name_list[-1]
-        name_list = driver.find_element_by_xpath(
+        name_list = driver.find_element('xpath',
             '//*[@id="content"]/div/div[2]/div/table/tbody/tr[{}]/td[7]'.format(i + 1)).text.split()
         first_name = name_list[0]
 
         if first_name != last_name:
-            print(driver.find_element_by_xpath(
+            print(driver.find_element('xpath',
                 '//*[@id="content"]/div/div[2]/div/table/tbody/tr[{}]/td[4]'.format(i + 1)).text, end="")
             print("일 잘못됨")
 
@@ -136,19 +136,16 @@ loginID_3 = "fks1114"
 
 # 로그인
 is_xpath_exist(driver, '//*[@id="f_code"]')
-driver.find_element_by_xpath('//*[@id="f_code"]').send_keys(loginID_1)
-driver.find_element_by_xpath('//*[@id="id"]').send_keys(loginID_2)
-driver.find_element_by_xpath('//*[@id="pwd"]').send_keys(loginID_3)
-driver.find_element_by_xpath('//*[@id="content"]/section[2]/div[1]/div[2]/div/div[3]/a[1]').click()
+driver.find_element('xpath', '//*[@id="f_code"]').send_keys(loginID_1)
+driver.find_element('xpath', '//*[@id="id"]').send_keys(loginID_2)
+driver.find_element('xpath', '//*[@id="pwd"]').send_keys(loginID_3)
+driver.find_element('xpath', '//*[@id="content"]/section[2]/div[1]/div[2]/div/div[3]/a[1]').click()
 
 # 경관식 대상자 클릭
 is_xpath_exist(driver, '//*[@id="mainmenu"]/ul/li[4]/ul/li[5]/a')
-driver.find_element_by_xpath('//*[@id="mainmenu"]/ul/li[4]/ul/li[5]/a').click()
+driver.find_element('xpath', '//*[@id="mainmenu"]/ul/li[4]/ul/li[5]/a').click()
 
-"""
-'강복순', '강순남', '강재현', '고연님', '김갑순', '김귀임', '김남석', '김단례', '김도순', '김분임', '김성순', '김애덕', '김춘선', '김춘자', '남정순', '문복선', '박당녀', '박형봉', '방남례', '서옥순', '송덕순', '송영자', '심삼남', '양명순', '양소례', '염옥순', '우복인', '유복', '유승숙', '이건주', '이상수', '이선미', '이얌순', '이양님', '이정애', '임계순', '임순덕', '전신자', '전점례', '정귀모', '정중헌', '정해님', '조대섭', '주정애', '진병금', '최순옥', '최정자', '탁봉순', '하태순', '한순임', '허경임', '허야무'
-"""
-name_list = ['강복순', '강순남', '강재현', '고연님', '김갑순', '김귀임', '김남석', '김단례', '김도순', '김분임', '김성순', '김애덕', '김춘선', '김춘자', '남정순', '문복선', '박당녀', '박형봉', '방남례', '서옥순', '송덕순', '송영자', '심삼남', '양명순', '양소례', '염옥순', '우복인', '유복', '유승숙', '이건주', '이상수', '이선미', '이얌순', '이양님', '이정애', '임계순', '임순덕', '전신자', '전점례', '정귀모', '정중헌', '정해님', '조대섭', '주정애', '진병금', '최순옥', '최정자', '탁봉순', '하태순', '한순임', '허경임', '허야무']
+name_list = get_name_list.get_name()
 
 for i in range(len(name_list)):
     name_auto_check(driver, name_list[i])
